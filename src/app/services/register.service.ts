a94181bffdcb5cfc068c5aca4f5d3af2a766355f
vinodhkumar2002 <vinodhkumarvk430@gmail.com>
2025-05-20 17:14:49 +0530

    updated environment  production file

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -11,8 +11,10 @@ export class RegisterService {
   constructor(private http:HttpClient) { }
 
   baseUrl:String=environment.backendApi
+  
 
   public register(user:User){
+    console.log("this is the backend url"+this.baseUrl)
     return this.http.post(this.baseUrl+"register",user);
   }
 
